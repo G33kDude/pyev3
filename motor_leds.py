89dcdf6a24163c380d8f0a20df5a08d24770549b
G33kDude <PtubbyTaylor@Gmail.com>
2014-12-07 22:09:46 -0500

    Modifed motor_leds.py to support the full range of brightness, and to find a tacho-motor on any port

--- motor_leds.py
+++ motor_leds.py
@@ -5,20 +5,28 @@ led_green_right = pyev3.LED('ev3:green:right')
 led_red_left = pyev3.LED('ev3:red:left')
 led_red_right = pyev3.LED('ev3:red:right')
 
-led_green_left.brightness = 100
-led_green_right.brightness = 100
+max_brightness = led_green_left.max_brightness
 
-tacho = pyev3.Motor(pyev3.OUTPUT_B)
+led_green_left.brightness = max_brightness
+led_green_right.brightness = max_brightness
+
+tacho = pyev3.Motor()
+print "Using motor {}".format(tacho.port_name)
+
 tacho.reset()
 
 tacho.position = 90
+adjusted = tacho.position
+last_position = adjusted
 
 while True:
-	position = tacho.position
-	adjusted = max(0, min(position, 180))
-	if adjusted != position:
-		tacho.position = adjusted
-	brightness = 100 * adjusted / 180
+	while adjusted == last_position:
+		position = tacho.position
+		adjusted = max(0, min(position, 180))
+		if adjusted != position:
+			tacho.position = adjusted
+	brightness = max_brightness * adjusted / 180
 	print adjusted, brightness
 	led_red_left.brightness = brightness
 	led_red_right.brightness = brightness
+	last_position = adjusted
